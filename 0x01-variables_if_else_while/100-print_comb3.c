60e245a34a81504775e76f4fff78e75ba845bd32
Sam <awasamuel85@yahoo.com>
2022-04-11 16:16:25 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+* main - Entry pont
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 9; i++)
+	{
+		for (j = 1; j < 10; j++)
+		{
+			if (i < j)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				if (i == 8 && j == 9)
+				{
+					putchar(10);
+				}
+				else
+				{
+					putchar(44);
+					putchar(32);
+				}
+			}
+		}
+	}
+	return (0);
+}
+			
